a22744f65b47fa03dca41ce6d4bb66c545d72eac
sahilpatel18 <sahil.41801@gmail.com>
2021-01-04 11:32:49 -0500

    requiring cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
@@ -8,7 +9,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
