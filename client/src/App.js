cc54adb3dec3097aa92b682ee7018973a2f7b3e2
sahilpatel18 <sahil.41801@gmail.com>
2021-01-04 11:32:22 -0500

    wrapping apollo provider in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import BookList from "./components/BookList";
+const URI = process.env.REACT_APP_GRAPH_URI;
+
+const client = new ApolloClient({
+  uri: URI,
+  cache: new InMemoryCache(),
+});
 
 class App extends React.Component {
   render() {
     return (
-      <div id="main">
-        <h1>Sahil</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Sahil</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
