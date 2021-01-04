5ec69d86794eff930c10cfb549d83141e39cc90d
sahilpatel18 <sahil.41801@gmail.com>
2021-01-04 11:33:58 -0500

    made query to retrieve books and display in a list

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-class BookList extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
-      </div>
-    );
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
   }
-}
+`;
+
+const BookList = () => {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div>
+      <ul id="book-list">
+        {data.books.map((book) => {
+          return (
+            <li key={book.id}>
+              {book.name} || {book.id}
+            </li>
+          );
+        })}
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
